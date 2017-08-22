553cabec6ca2259fcea33124c69e302f276678d3
Vitaly Nokhrin <vin@crtweb.ru>
2017-08-22 18:02:56 +0500

    Сделал разбор параметров и подготовку файла конфига

--- core/HostManager.php
+++ core/HostManager.php
@@ -47,11 +47,12 @@ class HostManager
         echo self::getColorCode('yellow') . 'Подробное описание основных команд и их параметров' . PHP_EOL;
         echo self::getColorCode('white') . 'create -dr:<DocumentRoot> -url:<URL> -cms:<bitrix>' . PHP_EOL;
         echo '  -dr:<DocumentRoot> - путь к папке с сайтом' . PHP_EOL;
-        echo '  -url:<URL> - адрес по которому бкдет доступен сайт' . PHP_EOL;
+        echo '  -cr:<ConfigurationRoot> - путь, где будут хранить конфиги хоста' . PHP_EOL;
+        echo '  -url:<URL> - адрес по которому будет доступен сайт' . PHP_EOL;
         echo '  -cms:<cms> - если передать 3м параметром слово bitrix, хост будет настроен для Bitrix' . PHP_EOL . PHP_EOL;
 
         echo self::getColorCode('yellow') . 'Пример:' . PHP_EOL;
-        echo self::getColorCode('default') . 'php manhost.php create /var/www/site.ru site.ru bitrix' . PHP_EOL;
+        echo self::getColorCode('default') . 'php hostman.php create /var/www/site.ru site.ru bitrix' . PHP_EOL;
 
         echo self::getColorCode('green') . '---------------------------------------------------------' . PHP_EOL . PHP_EOL;
         echo self::getColorCode('default');
@@ -63,11 +64,58 @@ class HostManager
     /**
      * Создать Хост
      * @param $arParams
+     * @return false
      */
     public static function createHost($arParams)
     {
-        // обрабатываем параметры
-        //if ()
+        $requiredParams = [
+            'doc_root' => '-dr',
+            'conf_system' => '-cr',
+            'url' => '-url',
+        ];
+
+        $useParams = [
+            'doc_root' => '-dr',
+            'conf_root' => '-cr',
+            'url' => '-url',
+            'cms' => '-cms',
+        ];
+
+        // парсим параметры
+        $arParams = HostManager::prepareParams($useParams, $arParams);
+
+        // проверяем заполненость обязательных параметров
+        if (!HostManager::checkRequireParams($requiredParams, $arParams)) {
+            return false;
+        }
+
+
+        // если cms не установлена, то задаем default
+        if (!isset($arParams['cms'])) {
+            $arParams['cms'] = 'default';
+        }
+
+        // копируем файл конфига хоста в /tmp/
+        $pathTemplateConf = __DIR__ . '/../files/' . $arParams['cms'] . '.conf';
+        $pathTmpConf = __DIR__ . '/../tmp/' . $arParams['url'] . '.conf';
+        copy($pathTemplateConf, $pathTmpConf);
+
+        // заменяем #docRoot# и #host# в файле конфига
+        $fileTmpConfContent = file_get_contents($pathTmpConf);
+        $fileTmpConfContent = str_replace('#host#', $arParams['url'], $fileTmpConfContent);
+        $fileTmpConfContent = str_replace('#document_root#', $arParams['doc_root'], $fileTmpConfContent);
+
+        // перемещаем файл в папку с настройками apache
+
+
+
+
+        echo '<pre>';
+        print_r($fileTmpConfContent);
+        echo '</pre>';
+
+
+        //return true;
     }
 
 
@@ -77,4 +125,45 @@ class HostManager
     }
 
 
+    /**
+     * Проверка заполненности обязательных параметров
+     * @param $arRequired
+     * @param $arParams
+     * @return bool
+     */
+    private static function checkRequireParams($arRequired, $arParams)
+    {
+        foreach ($arRequired as $key => $val) {
+            if (!isset($arParams[$key])) {
+                echo HostManager::getColorCode('red') . 'Не задан обязательный параметр ' . $val . PHP_EOL;
+                echo HostManager::getColorCode('default');
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    /**
+     * Разбирает параметры во входящей строке
+     * @param $arUseParams
+     * @param $arParams
+     * @return array
+     */
+    private static function prepareParams($arUseParams, $arParams)
+    {
+        $result = [];
+
+        foreach ($arUseParams as $keyUse => $valUse) {
+            foreach ($arParams as $keyPar => $valPar) {
+                if (strpos($valPar, $valUse) !== false) {
+                    $result[$keyUse] = str_replace($valUse . ':', '', $valPar);
+                    break;
+                }
+            }
+        }
+
+        return $result;
+    }
 }
